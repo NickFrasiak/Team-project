b6481abaffe5c2ffb9668c9588d0c97f1a1f3d47
Nicholas Frasiak <000959762@mohawkcollege.ca>
2024-11-19 16:10:34 -0500

    add sky class

---
+++ Group-3-Game/Sky.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game10003;
+
+internal class Sky
+{
+    Vector2 Position;
+
+
+    public Sky()
+    {
+        Position = new Vector2(0, 0);
+    }
+    public void Update()
+    {
+
+    }
+
+}
